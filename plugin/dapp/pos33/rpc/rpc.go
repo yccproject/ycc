3e3047e34d64572424612ed2813eebd6151cb5c7
wch <wangchh@gmail.com>
2022-05-13 11:23:14 +0800

    add pos33 info cmd

--- plugin/dapp/pos33/rpc/rpc.go
+++ plugin/dapp/pos33/rpc/rpc.go
@@ -12,22 +12,71 @@ import (
 	"golang.org/x/net/context"
 )
 
-// SetAutoMining set auto mining
-func (g *channelClient) SetAutoMining(ctx context.Context, in *ty.Pos33MinerFlag) (*types.Reply, error) {
-	data, err := g.ExecWalletFunc(ty.Pos33TicketX, "WalletAutoMiner", in)
-	if err != nil {
-		return nil, err
-	}
-	return data.(*types.Reply), nil
-}
-
-// GetAllPos33TicketCount get count
-func (g *channelClient) GetAllPos33TicketCount(ctx context.Context, in *types.ReqNil) (*types.Int64, error) {
-	msg, err := g.Query(ty.Pos33TicketX, "AllPos33TicketCount", in)
+// GetAllPos33TicketAmount get count
+func (g *channelClient) getAllPos33TicketAmount(ctx context.Context, in *types.ReqNil) (*types.Int64, error) {
+	msg, err := g.Query(ty.Pos33TicketX, "AllPos33TicketAmount", in)
 	if err != nil {
 		return nil, err
 	}
 	return msg.(*types.Int64), nil
+}
+
+// // GetAllPos33TicketAmount get count
+// func (j *Jrpc) GetAllPos33TicketAmount(in *types.ReqNil) (*types.Int64, error) {
+// 	a, err := j.cli.GetAllPos33TicketAmount(context.Background(), in)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return a, nil
+// }
+
+// GetPos33Info get pos33 ticket price and all ticket count
+func (c *Jrpc) GetPos33Info(in *types.ReqNil, result *interface{}) error {
+	resp, err := c.cli.GetPos33Info(context.Background(), in)
+	if err != nil {
+		return err
+	}
+	*result = resp
+	return nil
+}
+
+// GetPos33Info get pos33 ticket price and all ticket count
+func (g *channelClient) GetPos33Info(ctx context.Context, in *types.ReqNil) (*ty.ReplyPos33Info, error) {
+	header, err := g.GetLastHeader()
+	if err != nil {
+		return nil, err
+	}
+	height := header.Height
+	cfg33 := g.GetConfig()
+	mp := ty.GetPos33MineParam(cfg33, height)
+	tprice := mp.GetTicketPrice()
+
+	useEntrust := cfg33.IsDappFork(height, ty.Pos33TicketX, "UseEntrust")
+	if useEntrust {
+		amount, err := g.getAllPos33TicketAmount(ctx, in)
+		if err != nil {
+			return nil, err
+		}
+		count := amount.Data / tprice
+		return &ty.ReplyPos33Info{Price: tprice, AllCount: count}, nil
+	}
+
+	msg, err := g.Query(ty.Pos33TicketX, "AllPos33TicketCount", in)
+	if err != nil {
+		return nil, err
+	}
+	return &ty.ReplyPos33Info{AllCount: msg.(*types.Int64).Data, Price: tprice}, nil
+}
+
+// GetPos33TicketCount get ticket count
+func (c *Jrpc) GetPos33TicketCount(in *types.ReqAddr, result *int64) error {
+	resp, err := c.cli.GetPos33TicketCount(context.Background(), in)
+	if err != nil {
+		return err
+	}
+	*result = resp.GetData()
+	return nil
+
 }
 
 // GetPos33TicketCount get count
@@ -95,167 +144,6 @@ func (g *channelClient) SetMinerFeeRate(ctx context.Context, in *ty.Pos33MinerFe
 	}
 	return data.(*types.ReplyHash), nil
 }
-
-// // ClosePos33Tickets close ticket
-// func (c *Jrpc) ClosePos33Tickets(in *ty.Pos33TicketClose, result *interface{}) error {
-// 	resp, err := c.cli.ClosePos33Tickets(context.Background(), in)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	*result = &rpctypes.ReplyHash{Hash: common.ToHex(resp.Hash)}
-// 	return nil
-// }
-
-// // ClosePos33Tickets close ticket
-// func (g *channelClient) ClosePos33Tickets(ctx context.Context, in *ty.Pos33TicketClose) (*types.ReplyHash, error) {
-// 	// inn := *in
-// 	data, err := g.ExecWalletFunc(ty.Pos33TicketX, "ClosePos33Tickets", in)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	return data.(*types.ReplyHash), nil
-// }
-
-// GetAllPos33TicketCount get ticket count
-func (c *Jrpc) GetAllPos33TicketCount(in *types.ReqNil, result *int64) error {
-	resp, err := c.cli.GetAllPos33TicketCount(context.Background(), in)
-	if err != nil {
-		return err
-	}
-	*result = resp.GetData()
-	return nil
-
-}
-
-// GetPos33TicketCount get ticket count
-func (c *Jrpc) GetPos33TicketCount(in *types.ReqAddr, result *int64) error {
-	resp, err := c.cli.GetPos33TicketCount(context.Background(), in)
-	if err != nil {
-		return err
-	}
-	*result = resp.GetData()
-	return nil
-
-}
-
-// CreateBindMiner create bind miner
-// func (c *Jrpc) CreateBindMiner(in *ty.ReqBindPos33Miner, result *interface{}) error {
-// 	reply, err := c.cli.CreateBindMiner(context.Background(), in)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	*result = reply
-// 	return nil
-// }
-
-// // SetAutoMining set auto mining
-// func (c *Jrpc) SetAutoMining(in *ty.Pos33MinerFlag, result *rpctypes.Reply) error {
-// 	resp, err := c.cli.SetAutoMining(context.Background(), in)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	var reply rpctypes.Reply
-// 	reply.IsOk = resp.GetIsOk()
-// 	reply.Msg = string(resp.GetMsg())
-// 	*result = reply
-// 	return nil
-// }
-
-// GetPos33Deposit get ticket list info
-// func (g *channelClient) GetPos33Deposit(ctx context.Context, in *types.ReqAddr) (*ty.Pos33DepositMsg, error) {
-// 	data, err := g.Query(ty.Pos33TicketX, "Pos33Deposit", in)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	return data.(*ty.Pos33DepositMsg), nil
-// }
-
-// // GetPos33Deposit get ticket list info
-// func (c *Jrpc) GetPos33Deposit(in *types.ReqAddr, result *interface{}) error {
-// 	resp, err := c.cli.GetPos33Deposit(context.Background(), in)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	*result = resp
-// 	return nil
-// }
-
-// // GetPos33TicketList get ticket list info
-// func (g *channelClient) GetPos33TicketReward(ctx context.Context, in *ty.Pos33TicketReward) (*ty.ReplyPos33TicketReward, error) {
-// 	data, err := g.QueryConsensusFunc(ty.Pos33TicketX, "GetPos33Reward", in)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	return data.(*ty.ReplyPos33TicketReward), nil
-// }
-
-// // GetPos33TicketList get ticket list info
-// func (c *Jrpc) GetPos33TicketReward(in *ty.Pos33TicketReward, result *interface{}) error {
-// 	resp, err := c.cli.GetPos33TicketReward(context.Background(), in)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	*result = resp
-// 	return nil
-// }
-
-/*
-func bindMiner(cfg *types.Chain33Config, param *ty.ReqBindPos33Miner) (*ty.ReplyBindPos33Miner, error) {
-	tBind := &ty.Pos33TicketBind{
-		MinerAddress:  param.BindAddr,
-		ReturnAddress: param.OriginAddr,
-	}
-	data, err := types.CallCreateTx(cfg, cfg.ExecName(ty.Pos33TicketX), "Tbind", tBind)
-	if err != nil {
-		return nil, err
-	}
-	hex := common.ToHex(data)
-	return &ty.ReplyBindPos33Miner{TxHex: hex}, nil
-}
-
-// CreateBindMiner 创建绑定挖矿
-func (g *channelClient) CreateBindMiner(ctx context.Context, in *ty.ReqBindPos33Miner) (*ty.ReplyBindPos33Miner, error) {
-	if in.BindAddr != "" {
-		err := address.CheckAddress(in.BindAddr, -1)
-		if err != nil {
-			return nil, err
-		}
-	}
-	err := address.CheckAddress(in.OriginAddr, -1)
-	if err != nil {
-		return nil, err
-	}
-
-	cfg := g.GetConfig()
-	if in.CheckBalance {
-		header, err := g.GetLastHeader()
-		if err != nil {
-			return nil, err
-		}
-		price := ty.GetPos33MineParam(cfg, header.Height).GetTicketPrice()
-		if price == 0 {
-			return nil, types.ErrInvalidParam
-		}
-		if in.Amount%price != 0 || in.Amount < 0 {
-			return nil, types.ErrAmount
-		}
-
-		getBalance := &types.ReqBalance{Addresses: []string{in.OriginAddr}, Execer: cfg.GetCoinExec(), AssetSymbol: "ycc", AssetExec: cfg.GetCoinExec()}
-		balances, err := g.GetCoinsAccountDB().GetBalance(g, getBalance)
-		if err != nil {
-			return nil, err
-		}
-		if len(balances) == 0 {
-			return nil, types.ErrInvalidParam
-		}
-		if balances[0].Balance < in.Amount+2*cfg.GetCoinPrecision() {
-			return nil, types.ErrNoBalance
-		}
-	}
-	return bindMiner(cfg, in)
-}
-*/
 
 // query consignor entrust info
 func (g *channelClient) GetPos33ConsignorEntrust(ctx context.Context, in *types.ReqAddr) (*ty.Pos33Consignor, error) {
--- plugin/dapp/pos33/types/pos33.pb.go
+++ plugin/dapp/pos33/types/pos33.pb.go
@@ -2931,6 +2931,61 @@ func (x *ReplyTxHex) GetTxHex() string {
 	return ""
 }
 
+type ReplyPos33Info struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Price    int64 `protobuf:"varint,1,opt,name=price,proto3" json:"price,omitempty"`
+	AllCount int64 `protobuf:"varint,2,opt,name=all_count,json=allCount,proto3" json:"all_count,omitempty"`
+}
+
+func (x *ReplyPos33Info) Reset() {
+	*x = ReplyPos33Info{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_pos33_proto_msgTypes[44]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *ReplyPos33Info) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*ReplyPos33Info) ProtoMessage() {}
+
+func (x *ReplyPos33Info) ProtoReflect() protoreflect.Message {
+	mi := &file_pos33_proto_msgTypes[44]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use ReplyPos33Info.ProtoReflect.Descriptor instead.
+func (*ReplyPos33Info) Descriptor() ([]byte, []int) {
+	return file_pos33_proto_rawDescGZIP(), []int{44}
+}
+
+func (x *ReplyPos33Info) GetPrice() int64 {
+	if x != nil {
+		return x.Price
+	}
+	return 0
+}
+
+func (x *ReplyPos33Info) GetAllCount() int64 {
+	if x != nil {
+		return x.AllCount
+	}
+	return 0
+}
+
 var File_pos33_proto protoreflect.FileDescriptor
 
 var file_pos33_proto_rawDesc = []byte{
@@ -3250,13 +3305,17 @@ var file_pos33_proto_rawDesc = []byte{
 	0x72, 0x73, 0x65, 0x6e, 0x74, 0x18, 0x02, 0x20, 0x01, 0x28, 0x05, 0x52, 0x0e, 0x66, 0x65, 0x65,
 	0x52, 0x61, 0x74, 0x65, 0x50, 0x65, 0x72, 0x73, 0x65, 0x6e, 0x74, 0x22, 0x22, 0x0a, 0x0a, 0x52,
 	0x65, 0x70, 0x6c, 0x79, 0x54, 0x78, 0x48, 0x65, 0x78, 0x12, 0x14, 0x0a, 0x05, 0x74, 0x78, 0x48,
-	0x65, 0x78, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x05, 0x74, 0x78, 0x48, 0x65, 0x78, 0x32,
-	0x44, 0x0a, 0x05, 0x70, 0x6f, 0x73, 0x33, 0x33, 0x12, 0x3b, 0x0a, 0x0f, 0x53, 0x65, 0x74, 0x50,
-	0x6f, 0x73, 0x33, 0x33, 0x45, 0x6e, 0x74, 0x72, 0x75, 0x73, 0x74, 0x12, 0x13, 0x2e, 0x74, 0x79,
-	0x70, 0x65, 0x73, 0x2e, 0x50, 0x6f, 0x73, 0x33, 0x33, 0x45, 0x6e, 0x74, 0x72, 0x75, 0x73, 0x74,
-	0x1a, 0x11, 0x2e, 0x74, 0x79, 0x70, 0x65, 0x73, 0x2e, 0x52, 0x65, 0x70, 0x6c, 0x79, 0x54, 0x78,
-	0x48, 0x65, 0x78, 0x22, 0x00, 0x42, 0x0a, 0x5a, 0x08, 0x2e, 0x2e, 0x2f, 0x74, 0x79, 0x70, 0x65,
-	0x73, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x65, 0x78, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x05, 0x74, 0x78, 0x48, 0x65, 0x78, 0x22,
+	0x43, 0x0a, 0x0e, 0x52, 0x65, 0x70, 0x6c, 0x79, 0x50, 0x6f, 0x73, 0x33, 0x33, 0x49, 0x6e, 0x66,
+	0x6f, 0x12, 0x14, 0x0a, 0x05, 0x70, 0x72, 0x69, 0x63, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x03,
+	0x52, 0x05, 0x70, 0x72, 0x69, 0x63, 0x65, 0x12, 0x1b, 0x0a, 0x09, 0x61, 0x6c, 0x6c, 0x5f, 0x63,
+	0x6f, 0x75, 0x6e, 0x74, 0x18, 0x02, 0x20, 0x01, 0x28, 0x03, 0x52, 0x08, 0x61, 0x6c, 0x6c, 0x43,
+	0x6f, 0x75, 0x6e, 0x74, 0x32, 0x44, 0x0a, 0x05, 0x70, 0x6f, 0x73, 0x33, 0x33, 0x12, 0x3b, 0x0a,
+	0x0f, 0x53, 0x65, 0x74, 0x50, 0x6f, 0x73, 0x33, 0x33, 0x45, 0x6e, 0x74, 0x72, 0x75, 0x73, 0x74,
+	0x12, 0x13, 0x2e, 0x74, 0x79, 0x70, 0x65, 0x73, 0x2e, 0x50, 0x6f, 0x73, 0x33, 0x33, 0x45, 0x6e,
+	0x74, 0x72, 0x75, 0x73, 0x74, 0x1a, 0x11, 0x2e, 0x74, 0x79, 0x70, 0x65, 0x73, 0x2e, 0x52, 0x65,
+	0x70, 0x6c, 0x79, 0x54, 0x78, 0x48, 0x65, 0x78, 0x22, 0x00, 0x42, 0x0a, 0x5a, 0x08, 0x2e, 0x2e,
+	0x2f, 0x74, 0x79, 0x70, 0x65, 0x73, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -3272,7 +3331,7 @@ func file_pos33_proto_rawDescGZIP() []byte {
 }
 
 var file_pos33_proto_enumTypes = make([]protoimpl.EnumInfo, 1)
-var file_pos33_proto_msgTypes = make([]protoimpl.MessageInfo, 45)
+var file_pos33_proto_msgTypes = make([]protoimpl.MessageInfo, 46)
 var file_pos33_proto_goTypes = []interface{}{
 	(Pos33Msg_Ty)(0),               // 0: types.Pos33Msg.Ty
 	(*Pos33Ticket)(nil),            // 1: types.Pos33Ticket
@@ -3319,9 +3378,10 @@ var file_pos33_proto_goTypes = []interface{}{
 	(*Pos33WithdrawReward)(nil),    // 42: types.Pos33WithdrawReward
 	(*Pos33MinerFeeRate)(nil),      // 43: types.Pos33MinerFeeRate
 	(*ReplyTxHex)(nil),             // 44: types.ReplyTxHex
-	nil,                            // 45: types.Pos33SortMap.SortMapEntry
-	(*types.Signature)(nil),        // 46: types.Signature
-	(*types.Block)(nil),            // 47: types.Block
+	(*ReplyPos33Info)(nil),         // 45: types.ReplyPos33Info
+	nil,                            // 46: types.Pos33SortMap.SortMapEntry
+	(*types.Signature)(nil),        // 47: types.Signature
+	(*types.Block)(nil),            // 48: types.Block
 }
 var file_pos33_proto_depIdxs = []int32{
 	24, // 0: types.Pos33TicketAction.topen:type_name -> types.Pos33TicketOpen
@@ -3340,15 +3400,15 @@ var file_pos33_proto_depIdxs = []int32{
 	6,  // 13: types.Pos33SortMsg.proof:type_name -> types.HashProof
 	7,  // 14: types.Pos33Sorts.sorts:type_name -> types.Pos33SortMsg
 	8,  // 15: types.Pos33VoteSorts.vote_sorts:type_name -> types.Pos33Sorts
-	46, // 16: types.Pos33Online.Sig:type_name -> types.Signature
-	47, // 17: types.Pos33BlockMsg.b:type_name -> types.Block
-	47, // 18: types.Pos33BlockMsg2.b:type_name -> types.Block
+	47, // 16: types.Pos33Online.Sig:type_name -> types.Signature
+	48, // 17: types.Pos33BlockMsg.b:type_name -> types.Block
+	48, // 18: types.Pos33BlockMsg2.b:type_name -> types.Block
 	13, // 19: types.Pos33BlockMsg2.vs:type_name -> types.Pos33VoteMsg
 	7,  // 20: types.Pos33VoteMsg.sort:type_name -> types.Pos33SortMsg
-	46, // 21: types.Pos33VoteMsg.sig:type_name -> types.Signature
+	47, // 21: types.Pos33VoteMsg.sig:type_name -> types.Signature
 	7,  // 22: types.Pos33SortsVote.my_sorts:type_name -> types.Pos33SortMsg
-	46, // 23: types.Pos33SortsVote.sig:type_name -> types.Signature
-	45, // 24: types.Pos33SortMap.sort_map:type_name -> types.Pos33SortMap.SortMapEntry
+	47, // 23: types.Pos33SortsVote.sig:type_name -> types.Signature
+	46, // 24: types.Pos33SortMap.sort_map:type_name -> types.Pos33SortMap.SortMapEntry
 	13, // 25: types.Pos33Votes.vs:type_name -> types.Pos33VoteMsg
 	17, // 26: types.Pos33MakerVotes.mvs:type_name -> types.Pos33Votes
 	7,  // 27: types.Pos33TicketMiner.sort:type_name -> types.Pos33SortMsg
@@ -3900,6 +3960,18 @@ func file_pos33_proto_init() {
 				return nil
 			}
 		}
+		file_pos33_proto_msgTypes[44].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*ReplyPos33Info); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	file_pos33_proto_msgTypes[1].OneofWrappers = []interface{}{
 		(*Pos33TicketAction_Topen)(nil),
@@ -3919,7 +3991,7 @@ func file_pos33_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_pos33_proto_rawDesc,
 			NumEnums:      1,
-			NumMessages:   45,
+			NumMessages:   46,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
