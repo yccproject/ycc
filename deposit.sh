6270a4e7f482a4152dbfd9a37447e08385b60c30
wch <wangchh@gmail.com>
2022-05-24 15:46:01 +0800

    for --signAddrType 2

--- deposit.sh
+++ deposit.sh
@@ -1,1 +1,1 @@
-./ycc-cli --rpc_laddr=http://localhost:9901 send coins transfer -a=$2 -t 0x79a69527dd0d62dcfd3eae90eb6c3134c57489eb -k $1 
+./ycc-cli --rpc_laddr=http://localhost:9901 send coins transfer -a=$2 -t 0x79a69527dd0d62dcfd3eae90eb6c3134c57489eb -k $1 --signAddrType 2
--- entrust.sh
+++ entrust.sh
@@ -1,1 +1,1 @@
-./ycc-cli --rpc_laddr=http://localhost:9901 send pos33 entrust -a $4 -e $1 -r $2 -k $3
+./ycc-cli --rpc_laddr=http://localhost:9901 send pos33 entrust -a $4 -e $1 -r $2 -k $3 --signAddrType 2
